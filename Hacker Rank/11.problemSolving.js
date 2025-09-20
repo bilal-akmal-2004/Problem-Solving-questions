b98861a594eb98576f564d25ebab8615357ac082
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-09-20 22:51:10 +0500

    New problem solved.

---
+++ Hacker Rank/11.problemSolving.js
@@ -0,0 +1,7 @@
+//Many of the challenges on HackerRank are difficult and assume that you already know the relevant algorithms. These tutorial challenges are different. They break down algorithmic concepts into smaller challenges so that you can learn the algorithm by solving them. They are intended for those who already know some programming, however. You could be a student majoring in computer science, a self-taught programmer, or an experienced developer who wants an active algorithms review. Here's a great place to learn by doing!
+for (let i = 0; i < arr.length; i++) {
+  if (V === arr[i]) {
+    console.log(i);
+    return i;
+  }
+}
