5f27334017c4040eda4b67ff32ae6bdddca53857
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-11-04 00:45:56 +0500

    New problem solved, yes!

--- Hacker Rank/36.problemSolving.js
+++ Hacker Rank/36.problemSolving.js
@@ -1,3 +1,14 @@
+//Question:
+// A password is considered strong if it satisfies the following criteria:
+// It has at least 6 characters.
+// It contains at least one digit.
+// It contains at least one lowercase English character.
+// It contains at least one uppercase English character.
+// It contains at least one special character. The special characters are: !@#$%^&*()-+
+// Given a string representing the password, find the minimum number of characters to add to make the password strong.
+
+//Solution:
+
 let password = "Hello123@";
 let count = 0;
 if (!/[A-Z]/.test(password)) {
@@ -18,3 +29,7 @@ if (password.length < 6) {
 }
 console.log(count);
 return count;
+// Output: 0
+// Explanation: The password "Hello123@" already meets all the criteria for a strong password.
+// Output: 0
+// Explanation: The password "Hello123@" already meets all the criteria for a strong password.
