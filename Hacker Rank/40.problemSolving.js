2c0f1fae814cbc7bb4f26aab8239589dbe85bb0b
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-11-12 17:48:18 +0500

    New problem solved.

--- Hacker Rank/40.problemSolving.js
+++ Hacker Rank/40.problemSolving.js
@@ -12,29 +12,3 @@ for (let j = 0; j < s.length; j++) {
 }
 
 return "NO";
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-
-// Explanation:
-// 1. We define the target word "hackerrank" and initialize a counter to track our position in this word.
-// 2. We iterate through each character in the input string `s`.
-// 3. If the current character matches the character in `correctWord` at the current `count` index, we increment `count`.
-// 4. If `count` reaches the length of `correctWord`, it means we have found all characters in order, so we return "YES".
-// 5. If we finish iterating through `s` without finding all characters, we return "NO".
-// Time Complexity is O(n) because we may need to check each character in the input string once.function hackerrankInString(s) {
-// Space Complexity is O(1) since we are using a fixed amount of extra space regardless of input size.
-// function hackerrankInString(s) {
-//   let correctWord = "hackerrank";
-//   let count = 0;
-
-//   for (let j = 0; j < s.length; j++) {
-//     if (s[j] === correctWord[count]) {
-//       count++;
-//     }
-//     if (count === correctWord.length) {
-//       return "YES";
-//     }
-//   }
-
-//   return "NO";
-// }
---
+++ Hacker Rank/41.problemSolving.js
@@ -0,0 +1,15 @@
+// Problem: Even or Odd
+// Write a JavaScript program that checks if a number is even or odd.
+// Example:
+// Input: 4 → Output: Even
+// Input: 7 → Output: Odd
+// Hint: Use the modulus operator (%) to check the remainder when dividing by 2.
+// Solution:
+
+let a = 10;
+
+if (a % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
